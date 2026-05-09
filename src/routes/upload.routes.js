02b84d61f27318ce3a65c2d4da2106d4b9144fb6
BiriyaniBoy <atlas.one.no1@gmail.com>
2026-05-09 19:00:44 +0530

    video and voice upload done

--- src/routes/upload.routes.js
+++ src/routes/upload.routes.js
@@ -1,5 +1,12 @@
 import { Router } from "express";
-import { uploadImage, uploadVideo } from "../controllers/auth.controller.js";
+import { 
+    uploadImage, 
+    uploadVideo, 
+    uploadVoice, 
+    deleteImage, 
+    deleteVideo, 
+    deleteVoice 
+} from "../controllers/auth.controller.js";
 import { upload } from "../middlewares/multer.middleware.js";
 
 const router = Router();
@@ -10,5 +17,10 @@ router.route("/upload-image")
     .post(upload.single("image"), uploadImage);
 
 router.route("/upload-video").post(upload.single("video"), uploadVideo);
+router.route("/upload-voice").post(upload.single("voice"), uploadVoice);
+
+router.route("/delete-image").delete(deleteImage);
+router.route("/delete-video").delete(deleteVideo);
+router.route("/delete-voice").delete(deleteVoice);
 
 export default router;
